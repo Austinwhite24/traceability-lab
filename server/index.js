b006221ad100005a08863ff332b50fa5c31fa318
Austin White <basketballking19aw@gmail.com>
2022-02-24 14:00:09 -0700

    2nd commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 
 const port = process.env.PORT || 4545
